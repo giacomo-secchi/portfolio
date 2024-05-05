50febad1051429cd156e31d0dcddee9ae2a18d63
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-05-05 12:28:28 +0200

    Add support for TranslatePress multilingual and translations

--- functions.php
+++ functions.php
@@ -29,5 +29,12 @@ if ( class_exists( 'Jetpack' ) ) {
 	$twenties->jetpack = require 'inc/class-twenties-jetpack.php';
 }
 
+/**
+ * Initialize TranslatePress - Multilingual compatibility.
+ */
+if (class_exists( 'TRP_Translate_Press' ) ) {
+	$twenties->translatepress = require 'inc/class-twenties-translatepress.php';
+}
+
 
 // require 'inc/class-twenties-ajax.php';
