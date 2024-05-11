75e89e63cd107b61c5237f55a74a98ab00a7c7d7
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-05-11 23:26:24 +0200

    chore: Update Jetpack portfolio query to show all posts and order by project year

--- inc/class-twenties-jetpack.php
+++ inc/class-twenties-jetpack.php
@@ -67,6 +67,9 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 			// The meta key would be the writepoetry_project_year field assigned to the jetpack-portfolio CPT
 			$query['meta_key'] = 'writepoetry_project_year';
 
+			// number of post to show per page. Use 'posts_per_page'=>-1 to show all posts
+			$query['posts_per_page'] = -1;
+
 			// also likely want to set order by this key in desc so more recent project are listed first.
 			$query['orderby'] = 'meta_value';
 			$query['order'] = 'desc';
