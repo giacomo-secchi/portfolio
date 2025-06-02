92eec04e992bed4b39abe2720d76ba12838aaee3
Alberto-Giusti <spaghetti_boys@hotmail.it>
2025-06-02 12:03:52 +0200

    chore: Update CSS variables for improved styling and remove unnecessary elements in HTML templates

--- inc/class-twenties-jetpack.php
+++ inc/class-twenties-jetpack.php
@@ -31,6 +31,9 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 		 * @since 1.0
 		 */
 		public function __construct() {
+
+			// Stampa tutti i post type registrati
+
 			add_filter( 'wp_body_open',  array( $this, 'popup_template' ), 10, 2 );
 
 			add_filter( 'render_block_core/read-more', array( $this, 'modify_readmore_blocks' ), 10, 2 );
@@ -41,8 +44,9 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 			add_filter( 'render_block_core/post-terms', array( $this, 'remove_taxonomy_links' ), 10, 2 );
 
 			add_filter( 'init', function () {
+
 				add_filter( 'pre_render_block',  array( $this, 'load_scripts' ), 10, 2 );
-				add_filter( 'rest_jetpack-portfolio_query', array( $this, 'rest_project_date' ), 10, 2 );
+				 
 			}, 10, 2 );
 		}
 
@@ -52,7 +56,7 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 				'main',
 				get_theme_file_uri( '/assets/js/main.js' ),
 				array( 'wp-api-fetch' ),
-				'1.0.0',
+				'1.1.0',
 				true
 			);
 		}
@@ -87,64 +91,6 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 			return $block_content;
 		}
 
-
-		public function rest_project_date( $args, $request ) {
-
-			// add same meta query arguments for rest api (backend).
-			$args = $this->filter_query();
-			return $args;
-		}
-
-
-		private function filter_query() {
-			$queried_object = get_queried_object();
-
-			if ( $queried_object instanceof WP_Term ) {
-				$query['tax_query'] = array(
-					'relation' => 'AND',
-					array(
-						'taxonomy' => get_queried_object()->taxonomy,
-						'field'    => 'slug',
-						'terms'    => get_queried_object()->slug,
-					),
-				);
-			}
-
-			$query['post_type'] = 'jetpack-portfolio';
-
-			// The meta key would be the writepoetry_project_year field assigned to the jetpack-portfolio CPT
-			$query['meta_key'] = 'writepoetry_project_year';
-
-			// Show all posts
-			$query['nopaging'] = true;
-
-			// Skip SQL_CALC_FOUND_ROWS for performance (no pagination).
-			$query['no_found_rows'] = true;
-
-			// also likely want to set order by this key in desc so more recent project are listed first.
-			$query['orderby'] = 'meta_value_num';
-			$query['order'] = 'desc';
-
-			return $query;
-		}
-
-		public function projects_pre_render_block( $pre_render, $parsed_block ) {
-			// Verify it's the block that should be modified using the namespace.
-			if ( isset( $parsed_block['attrs']['namespace'] )  && 'jetpack/projects-list' === $parsed_block['attrs']['namespace'] ) {
-				// Filters the arguments which will be passed to WP_Query for the Query Loop Block.
-				add_filter( 'query_loop_block_query_vars',
-					function( $query, $block ) use ( $parsed_block ) {
-						// add same meta query arguments for front-end.
-						$query = $this->filter_query();
-                		return $query;
-					},
-					10,
-					2
-				);
-			}
-
-		  	return $pre_render;
-		}
  
 		public function get_project_types( $source_args, $block_instance, $attribute_name ) {
 			// If no key or user ID argument is set, bail early.
@@ -198,16 +144,16 @@ if ( ! class_exists( 'Twenties_Child_Jetpack' ) ) :
 			check_ajax_referer( 'popup_nonce', 'nonce' );
 			
 			$project_id = intval( $_POST['project_id'] );
-			$project = get_post($project_id);
+			$project = get_post( $project_id );
 
-			if (!$project) {
-				wp_send_json_error(['message' => 'Project not found'], 404);
+			if ( ! $project ) {
+				wp_send_json_error( ['message' => 'Project not found'], 404 );
 			}
 
-			wp_send_json_success([
-				'title'   => get_the_title($project),
-				'content' => apply_filters('the_content', $project->post_content)
-			]);
+			wp_send_json_success( [
+				'title'   => get_the_title( $project ),
+				'content' => apply_filters( 'the_content', $project->post_content )
+			] );
 		}
 	}
 endif;
